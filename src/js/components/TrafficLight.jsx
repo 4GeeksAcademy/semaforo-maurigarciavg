e2bb00acb552aa1469cc6bf7d434b02a19527308
Mauri García <63955780+maurigarciavg@users.noreply.github.com>
2026-02-03 18:46:38 +0000

    terminado

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 
 const TrafficLight = () => {
-    
+
     const [selected, setSelected] = useState("red");
-    
+
     const [lights, setLights] = useState(["red", "orange", "green"]);
+
+    const addPurple = () => {
+
+        if (!lights.includes("purple")) {
+            setLights([...lights, "purple"]);
+        } else {
+            null
+        }
+    };
 
     return (
         <div className="scene">
             <div className="hanging-traffic-light">
                 <div className="cable"></div>
                 <div className="traffic-light-box">
-                    
+
                     {lights.map((color) => {
                         return (
                             <div
@@ -28,19 +37,17 @@ const TrafficLight = () => {
                 <button
                     className="lightChange"
                     onClick={() => {
-                        if (selected === "red") {
-                            setSelected("green");
-                        } else if (selected === "green") {
-                            setSelected("orange");
-                        } else if (selected === "orange") {
-                            setSelected("red");
-                        }
+                       const currentIndex = lights.indexOf(selected);
+                        const nextIndex = (currentIndex + 1) % lights.length;
+                        setSelected(lights[nextIndex]);
                     }}
                 >
                     Pulsar para cruzar
                 </button>
+
+                <button className="addPurple" onClick={addPurple}>Añadir morado</button>
             </div>
-        </div>
+        </div >
     );
 };
 
